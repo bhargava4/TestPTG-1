b0b87669fa3bd7f1f421542ec8d8222cd1f37727
Keerthi <Keerthi@Admin>
2015-05-04 12:26:03 -0700

    Name and Contact changes - Keerthi

--- AboutMeDemo/src/main/java/com/trinet/aboutme/dao/EmployeeInfoDAO.java
+++ AboutMeDemo/src/main/java/com/trinet/aboutme/dao/EmployeeInfoDAO.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import com.trinet.aboutme.beans.Address;
 import com.trinet.aboutme.beans.Contact;
-
+import com.trinet.aboutme.beans.Name;
 public interface EmployeeInfoDAO {
 	
 	List<Address> getAddress(Integer employeeId);
 	
 	List<Contact> getContact(Integer employeeId);
 
-	
+	List<Name> getName(Integer employeeId);
+
+
 }
