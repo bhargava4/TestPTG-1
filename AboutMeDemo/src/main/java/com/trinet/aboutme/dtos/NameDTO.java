b948c95291daf104cb3fb14eca48121af665d8e9
Shannu <Shannu@172.20.2.239>
2015-05-03 22:33:17 -0700

    localcommit - shravan

---
+++ AboutMeDemo/src/main/java/com/trinet/aboutme/dtos/NameDTO.java
@@ -0,0 +1,105 @@
+/**
+ * 
+ */
+package com.trinet.aboutme.dtos;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+
+/**
+ * @author Shannu
+ *
+ */
+public class NameDTO {
+
+	private String name;
+
+	private int employeeID;
+
+	private Date effectiveDate;
+
+	private String formOfAddress;
+
+	private String firstName;
+
+	private String middleName;
+
+	private String suffix;
+
+	private String lastName;
+
+	private String nameType;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getEmployeeID() {
+		return employeeID;
+	}
+
+	public void setEmployeeID(int employeeID) {
+		this.employeeID = employeeID;
+	}
+
+	public Date getEffectiveDate() {
+		return effectiveDate;
+	}
+
+	public void setEffectiveDate(Date effectiveDate) {
+		this.effectiveDate = effectiveDate;
+	}
+
+	public String getFormOfAddress() {
+		return formOfAddress;
+	}
+
+	public void setFormOfAddress(String formOfAddress) {
+		this.formOfAddress = formOfAddress;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	public String getSuffix() {
+		return suffix;
+	}
+
+	public void setSuffix(String suffix) {
+		this.suffix = suffix;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getNameType() {
+		return nameType;
+	}
+
+	public void setNameType(String nameType) {
+		this.nameType = nameType;
+	}	
+}
